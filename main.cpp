74d0250698cf8aad460076f9ac09439130b39cad
n0kk <n0kk@cyberforcesec.com.br>
2024-11-03 18:31:09 -0300

    Amass and nmap scanning

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 #include <string>
 #include <cstdlib>
 
+void runAmass(const std::string& domain, const std::string& outputFile) {
+    // Build the Amass command to run
+    std::string command = "amass enum -d " + domain + " -o " + outputFile; // Specify output file
+    std::cout << "Executing: " << command << std::endl;
+    // Execute the Amass command
+    std::system(command.c_str());
+}
+
 void scanHosts(const std::string& filename, const std::string& scanType, const std::string& outputFile) {
     std::ifstream file(filename);
     if (!file.is_open()) {
@@ -13,10 +21,10 @@ void scanHosts(const std::string& filename, const std::string& scanType, const s
     std::string host;
     while (std::getline(file, host)) {
         if (!host.empty()) {
-            // Build the Nmap command with the scan type and output
-            std::string command = "nmap " + scanType + " -v " + host + " -oN " + outputFile;
+            // Build the Nmap command with the scan type and append output
+            std::string command = "nmap " + scanType + " -v " + host + " >> " + outputFile; // Append output with >>
             std::cout << "Executing: " << command << std::endl;
-            // Execute the command
+            // Execute the Nmap command
             std::system(command.c_str());
         }
     }
@@ -60,6 +68,19 @@ int main(int argc, char* argv[]) {
             return 1;
     }
 
+    // Run Amass enumeration before the Nmap scan
+    // Read the first line of the hosts file to get the domain
+    std::ifstream hostFile(filename);
+    std::string domain;
+    if (std::getline(hostFile, domain)) {
+        runAmass(domain, outputFile); // Run Amass with the domain
+    } else {
+        std::cerr << "No domain found in the hosts file: " << filename << std::endl;
+        return 1;
+    }
+    hostFile.close();
+
+    // Proceed to scan hosts with Nmap
     scanHosts(filename, nmapScanType, outputFile);
 
     return 0;
